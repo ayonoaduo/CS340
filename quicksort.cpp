f691111354dabad86ed437d1a8e4c10a2d1d5bf7
Oni <ayonoaduo@gmail.com>
2020-08-10 21:39:15 -0600

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -5,3 +5,8 @@ Course: CS 340 - Advanced Data Structures
 Task: This program will will sort any number of elements using the Quicksort algorithm
 **/
 
+#include <iostream>
+using namespace std;
+
+int partition(int *,int, int);
+void Quicksort(int *,int, int);
