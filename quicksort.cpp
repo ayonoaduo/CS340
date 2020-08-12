2db46eecdf3a62e387c2397ef46dbfb9a8b47e73
Oni <ayonoaduo@gmail.com>
2020-08-11 18:13:14 -0600

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,9 +1,9 @@
-/**
+/*
 Student Name: Onisokien Ayonoadu
 Student Number: 200360422
 Course: CS 340 - Advanced Data Structures
 Task: This program will will sort any number of elements using the Quicksort algorithm
-**/
+*/
 
 #include <iostream>
 using namespace std;
